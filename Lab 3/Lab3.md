7237f52f6c5d2b87cdc67eaae11f96c321a6e5cf
Keith <brian.skinner121@myci.csuci.edu>
2019-10-08 03:30:32 -0700

    Stage 3 Complete

--- Lab 3/Lab3.md
+++ Lab 3/Lab3.md
@@ -26,7 +26,7 @@ Setup
     STR  R0, [R1]                   ; Set enable register as new value
 ```
 
-Delay
+Delay Calculation
 
 $$ \frac{80Mhz}{1{sec}} \cdot \frac{1{sec}}{4\space{toggles}} \cdot \frac{1\space{toggle}}{4\space{cycles}} = 5000000{cycles}\space{per}\space{wait}  $$
 
@@ -35,6 +35,8 @@ $$ 5000000_{10} = 0x004C4B40_{16} $$
 When attempting to move `0x4C4B40` into register 0, the compiler complains:
 ```main.s(82): error: A1871E: Immediate 0x004C4B40 cannot be represented by 0-255 shifted left by 0-23 or duplicated in all, odd or even bytes```
 So I had to move half the value in (`0x4C`) then move the other half in after (`0x4B40`).
+
+### Delay
 
 ```nasm
 delay
@@ -47,5 +49,147 @@ wait
     BX   LR                         ; return
 ```
 
-Main
+### Flip PE3
 
+```nasm
+flip_PE3
+    LDR  R1, =GPIO_PORTE_DATA_R     ; Grab the address for the DATA register for Port E
+    LDR  R0, [R1]                   ; Grab the value currently held by the DATA register for Port E
+    EOR  R0, R0, #0x08              ; Flip the 4th bit for LED
+    STR  R0, [R1]                   ; Store the new data value
+    BX   LR                         ; return
+```
+
+### Main
+
+```nasm
+loop
+    BL   delay
+    BL   flip_PE3
+    B    loop
+```
+
+## Step 2
+
+Changes: Moved the delay length to
+```nasm
+LAB3_STAGE_1_25_PERC EQU 0x4C4B40   ; 5,000,000
+```
+
+And added the S
+```nasm
+LAB3_STAGE_2_15_PERC EQU 0x2DC6C0   ; 3,000,000
+LAB3_STAGE_2_35_PERC EQU 0x6ACFC0   ; 7,000,000
+```
+
+Then changed the delay function to use R0 as its first parameter.
+
+### Delay
+
+```nasm
+delay
+    SUBS R0, R0, #0x01              ; Subtract 1 from R0 and set zero flag(1 cycle)(SUB does not set zero flag)
+    BNE  delay                      ; Continue delay if R0 is not 0(3 cycles)
+    BX   LR                         ; return
+```
+
+### Main
+
+```nasm
+loop  
+    ; main engine goes here
+    LDR  R0, =LAB3_STAGE_2_35_PERC  ; 7/20 of a second
+    BL   delay                      ;
+    BL   flip_PE3                   ;
+    LDR  R0, =LAB3_STAGE_2_15_PERC  ; 3/20 of a second
+    BL   delay                      ;
+    BL   flip_PE3                   ;
+    B    loop                       ;
+```
+
+## Step 3 
+
+I chose to make my loop increment the duty cycle by 20 percent each time PE2 was pressed and released and only acted on release. Also I chose to end the current delay when doing so, causing odd spikes, but I wanted to see results immidiately.
+
+To do this I had to rework my delay function quite a bit. One problem I ran into was calling subroutines inside subroutines. This made my delay function act irratically. This is something I need to ask the teacher about for future labs. I ended up inlining all my calls to subroutines and it got messy, especially since the structure is still there.
+
+### Delay
+
+```nasm
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; R0 = How much to delay by
+; R1 = value of data register
+; R2 = address of data register
+; R3 = Unused
+; R4 = Whether or not the button on PE2 was pressed
+;   R4:0 is set while pressed
+;   R4:1 is set when pressed
+;   R4:0 is unset when released
+;   R4: 0b0000 -> not pressed
+;       0b0011 -> being pressed
+;       0b0010 -> was pressed
+; R5 = Current Duty Cycle
+; R6 = How much to add when the button is pressed
+; R7 = 100% duty cycle
+delay
+button_state
+    LDR  R2, =GPIO_PORTE_DATA_R     ; Get the address of the data register
+    LDR  R1, [R2]                   ; Get the value of the data register
+    AND  R1, R1, #0x4               ; Isolate PE2
+    CMP  R1, #0x4                   ; Is PE2 on?
+    BEQ  button_state_pressed       ; PE2 is pressed
+    AND  R4, R4, #0x2               ; PE2 not pressed -> turn off first bit in R4
+    B    button_state_done          ; done 
+button_state_pressed                
+    ORR  R4, R4, #0x3               ; turn on the first 2 bits
+button_state_done
+update_duty                         
+    AND R4, R4, #3                  ; Zero out all but the first 2 bits (not really neccessary is it?)
+    CMP R4, #2                      ; Is the value currently 2
+    BNE update_duty_done            ; If R4 != 2 then button not yet pressed or still being pressed
+increment
+    MOV R4, #0                      ; Clear R4
+    ADD  R5, R5, R6                 ; Add 20% to the duty cycle
+    CMP  R5, R7                     ; Is the duty cycle <= 100
+    BLE  increment_done             ; We done
+    SUB  R5, R5, R7                 ; duty cycle -= 100
+increment_done
+    MOV R0, #1                      ; Set R0 to no more delay
+update_duty_done                    
+    SUBS R0, R0, #5                 ; Subtract 5 from R0 and set zero flag(SUB does not set zero flag)
+    BGT  delay                      ; Continue delay if R0 is not 0
+    BX   LR                         ; return
+```
+
+
+### Main
+
+Although not featured here, the setup still lives above this code.
+
+```nasm
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; R0 = argument to delay
+; R4 = Whether or not a button was pushed
+; R5 = Current Duty Cycle
+; R6 = How much to add when the button is pressed
+; R7 = 100% duty cycle
+main
+    MOV  R4, #0                     ; Default value of button being pushed
+    LDR  R5, =LAB3_STEP_3_30_PERC   ; DUTY CYCLE : Default duty cycle 30%@2Hz
+    LDR  R6, =LAB3_STEP_3_20_PERC   ; DUTY CYCLE STEP : When button pressed, add this to R4
+    LDR  R7, =LAB3_STEP_3_100_PERC  ; DUTY CYCLE MOD : Max Duty Cycle 100%@2Hz
+loop                                ; ~~~~~~MAIN ENGINE GOES HERE~~~~~~~~
+    BL   flip_PE3                   ; Flip the LED on
+    MOV  R0, R5                     ; Set delay to the duty cycle
+    BL   delay                      ; Delay (LED on)
+    BL   flip_PE3                   ; Flip the LED (LED off)
+    SUB  R0, R7, R5                 ; Set the delay to the duty cycle minus the total time
+    BL   delay                      ; Delay (LED off)
+    B    loop                       ; repeat
+```
+
+## Step 4
+
+Although I was able to set up the circuit and the effect was essentially the same. The timing was off as expected. By how much I didn't measure.
+
+## Step 5
--- Lab3_EE319K/main.s
+++ Lab3_EE319K/main.s
@@ -2,35 +2,13 @@
 ; Program written by: Keith Skinner
 ; Date Created: 2/4/2017
 ; Last Modified: 9/2/2019
-; Brief description of the program
-;   The LED toggles at 2 Hz and a varying duty-cycle
-; Hardware connections (External: One button and one LED)
-;  PE1 is Button input  (1 means pressed, 0 means not pressed)
-;  PE2 is LED output (1 activates external LED on protoboard)
-;  PF4 is builtin button SW1 on Launchpad (Internal) 
-;        Negative Logic (0 means pressed, 1 means not pressed)
-; Overall functionality of this system is to operate like this
-;   1) Make PE2 an output and make PE1 and PF4 inputs.
-;   2) The system starts with the the LED toggling at 2Hz,
-;      which is 2 times per second with a duty-cycle of 30%.
-;      Therefore, the LED is ON for 150ms and off for 350 ms.
-;   3) When the button (PE1) is pressed-and-released increase
-;      the duty cycle by 20% (modulo 100%). Therefore for each
-;      press-and-release the duty cycle changes from 30% to 70% to 70%
-;      to 90% to 10% to 30% so on
-;   4) Implement a "breathing LED" when SW1 (PF4) on the Launchpad is pressed:
-;      a) Be creative and play around with what "breathing" means.
-;         An example of "breathing" is most computers power LED in sleep mode
-;         (e.g., https://www.youtube.com/watch?v=ZT6siXyIjvQ).
-;      b) When (PF4) is released while in breathing mode, resume blinking at 2Hz.
-;         The duty cycle can either match the most recent duty-
-;         cycle or reset to 30%.
-;      TIP: debugging the breathing LED algorithm using the real board.
+
 ; PortE device registers
 GPIO_PORTE_DATA_R  EQU 0x400243FC
 GPIO_PORTE_DIR_R   EQU 0x40024400
 GPIO_PORTE_AFSEL_R EQU 0x40024420
 GPIO_PORTE_DEN_R   EQU 0x4002451C
+    
 ; PortF device registers
 GPIO_PORTF_DATA_R  EQU 0x400253FC
 GPIO_PORTF_DIR_R   EQU 0x40025400
@@ -39,57 +17,116 @@ GPIO_PORTF_PUR_R   EQU 0x40025510
 GPIO_PORTF_DEN_R   EQU 0x4002551C
 GPIO_PORTF_LOCK_R  EQU 0x40025520
 GPIO_PORTF_CR_R    EQU 0x40025524
-GPIO_LOCK_KEY      EQU 0x4C4F434B  ; Unlocks the GPIO_CR register
+GPIO_LOCK_KEY      EQU 0x4C4F434B   ; Unlocks the GPIO_CR register
 SYSCTL_RCGCGPIO_R  EQU 0x400FE608
+
+; Lab related constants
+;LAB3_STEP_1_50_PERC  EQU  5000000
+;LAB3_STEP_2_30_PERC  EQU  3000000
+;LAB3_STEP_2_70_PERC  EQU  7000000
+
+; Lab constants for current step
+LAB3_STEP_3_30_PERC  EQU  3000000
+LAB3_STEP_3_20_PERC  EQU  2000000
+LAB3_STEP_3_100_PERC EQU 10000000
 
     IMPORT  TExaS_Init
     THUMB
     AREA    DATA, ALIGN=2
     ;global variables go here
-
-
     AREA    |.text|, CODE, READONLY, ALIGN=2
     THUMB
     EXPORT  Start
+        
 Start
+setup
     ; TExaS_Init sets bus clock at 80 MHz
-    BL  TExaS_Init ; voltmeter, scope on PD3
-    ; Initialization goes here
-    
-    ; Begin Keith Section
+    BL  TExaS_Init                  ; voltmeter, scope on PD3
     ; Turn on clock for port E
     LDR  R1, =SYSCTL_RCGCGPIO_R     ; Grab clock location
 	LDR  R0, [R1]                   ; Grab clock value
-	MOV  R0, #0x10                  ; Bit 5 is for Port E
+	ORR  R0, #0x10                  ; Bit 5 is for Port E
 	STR  R0, [R1]                   ; Store clock values turning on E
 	NOP                             ; Wait part1
 	NOP                             ; Wait part2
     ; Set PE3 as output
     LDR  R1, =GPIO_PORTE_DIR_R      ; Grab direction location
     LDR  R0, [R1]                   ; Grab direction value
-	MOV  R0, #0x8                   ; PE3 as output
+	ORR  R0, #0x8                   ; PE3 as output
+    AND  R0, #0xFFFFFFFB            ; PE2 as input
 	STR  R0, [R1]                   ; Store configuration
     ; Set PE3 as digital
     LDR  R1, =GPIO_PORTE_DEN_R      ; Grab enable register address
     LDR  R0, [R1]                   ; Grab enable register's value
-    MOV  R0, #0x8                   ; Set PE3 Pin as enabled
+    ORR  R0, #0xC                   ; Set PE3 and PE2 Pin as enabled
     STR  R0, [R1]                   ; Set enable register as new value
+    ; TExaS voltmeter, scope runs on interrupts
+    CPSIE  I                        ; Turn on TExaS voltmeter
+
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; R0 = argument to delay
+; R4 = Whether or not a button was pushed
+; R5 = Current Duty Cycle
+; R6 = How much to add when the button is pressed
+; R7 = 100% duty cycle
+main
+    MOV  R4, #0                     ; Default value of button being pushed
+    LDR  R5, =LAB3_STEP_3_30_PERC   ; DUTY CYCLE : Default duty cycle 30%@2Hz
+    LDR  R6, =LAB3_STEP_3_20_PERC   ; DUTY CYCLE STEP : When button pressed, add this to R4
+    LDR  R7, =LAB3_STEP_3_100_PERC  ; DUTY CYCLE MOD : Max Duty Cycle 100%@2Hz
+loop                                ; ~~~~~~MAIN ENGINE GOES HERE~~~~~~~~
+    BL   flip_PE3                   ; Flip the LED on
+    MOV  R0, R5                     ; Set delay to the duty cycle
+    BL   delay                      ; Delay (LED on)
+    BL   flip_PE3                   ; Flip the LED (LED off)
+    SUB  R0, R7, R5                 ; Set the delay to the duty cycle minus the total time
+    BL   delay                      ; Delay (LED off)
+    B    loop                       ; repeat
     
-    CPSIE  I    ; TExaS voltmeter, scope runs on interrupts
-loop  
-    ; main engine goes here
-    BL delay
-    BL flip_PE3
-    B    loop
-    
+
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; R0 = How much to delay by
+; R1 = value of data register
+; R2 = address of data register
+; R3 = Unused
+; R4 = Whether or not the button on PE2 was pressed
+;   R4:0 is set while pressed
+;   R4:1 is set when pressed
+;   R4:0 is unset when released
+;   R4: 0b0000 -> not pressed
+;       0b0011 -> being pressed
+;       0b0010 -> was pressed
+; R5 = Current Duty Cycle
+; R6 = How much to add when the button is pressed
+; R7 = 100% duty cycle
 delay
-    MOV  R1, #0x4C                  ; Move high half of Dword into R1
-    MOV  R0, #0x4B40                ; Move low half of Dword into R0
-    ADD  R0, R0, R1, LSL #0x10      ; Merge the two
-delay_wait
-    SUBS R0, R0, #0x01              ; (1 cycle)
-    BNE  delay_wait                       ; (3 cycles)
-    BX   LR                         ; return
+button_state
+    LDR  R2, =GPIO_PORTE_DATA_R     ; Get the address of the data register
+    LDR  R1, [R2]                   ; Get the value of the data register
+    AND  R1, R1, #0x4               ; Isolate PE2
+    CMP  R1, #0x4                   ; Is PE2 on?
+    BEQ  button_state_pressed       ; PE2 is pressed
+    AND  R4, R4, #0x2               ; PE2 not pressed -> turn off first bit in R4
+    B    button_state_done          ; done 
+button_state_pressed                
+    ORR  R4, R4, #0x3               ; turn on the first 2 bits
+button_state_done
+update_duty                         
+    AND R4, R4, #3                  ; Zero out all but the first 2 bits (not really neccessary is it?)
+    CMP R4, #2                      ; Is the value currently 2
+    BNE update_duty_done            ; If R4 != 2 then button not yet pressed or still being pressed
+increment
+    MOV R4, #0                      ; Clear R4
+    ADD  R5, R5, R6                 ; Add 20% to the duty cycle
+    CMP  R5, R7                     ; Is the duty cycle <= 100
+    BLE  increment_done             ; We done
+    SUB  R5, R5, R7                 ; duty cycle -= 100
+increment_done
+    MOV R0, #1                      ; Set R0 to no more delay
+update_duty_done                    
+    SUBS R0, R0, #5                ; Subtract 5 from R0 and set zero flag(1 cycle)(SUB does not set zero flag)
+    BGT  delay                      ; Continue delay if R0 is not 0(3 cycles)
+    BX   LR                         ; return (total 4 cycles)
     
 flip_PE3
     LDR  R1, =GPIO_PORTE_DATA_R     ; Grab the address for the DATA register for Port E
@@ -98,7 +135,5 @@ flip_PE3
     STR  R0, [R1]                   ; Store the new data value
     BX   LR                         ; return
     
-
     ALIGN      ; make sure the end of this section is aligned
     END        ; end of file
-
